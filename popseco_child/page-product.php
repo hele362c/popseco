093d93d3fa9bb54fc4f044cc6b3a29da2b6859d8
Helena Jakobsen <helenajakobsen@helenas-air-4.lan>
2021-05-29 02:35:12 +0200

    css på product siden

--- popseco_child/page-product.php
+++ popseco_child/page-product.php
@@ -1,4 +1,5 @@
-<?php
+<div id="produktside">
+    <?php
 /**
  * The template for displaying all pages.
  *
@@ -10,91 +11,93 @@ $container_class = apply_filters( 'neve_container_class_filter', 'container', 's
 get_header();
 
 ?>
-<div class="<?php echo esc_attr( $container_class ); ?> single-page-container">
+    <div class="<?php echo esc_attr( $container_class ); ?> single-page-container">
 
-    <head>
-    </head>
-    <template>
-        <article>
-            <img src="" alt="" class="billede">
-            <h2 class="isensnavn"></h2>
-            <h3 class="pris"></h3>
-        </article>
-    </template>
+        <head>
+        </head>
+        <h1>THE FREEZERS</h1>
+        <template>
+            <article>
+                <img src="" alt="" class="billede">
+                <h2 class="isensnavn"></h2>
+                <h3 class="pris"></h3>
+            </article>
+        </template>
 
-    <section id="primary" class="content-area">
-        <main id="main" class="site-main">
-            <nav id="filtrering">
-                <button data-is="alle">alle</button>
-            </nav>
-            <section class="iscontainer"></section>
-        </main>
-        <script>
-            let isene;
-            let categories;
-            let filterIs = "alle";
+        <section id="primary" class="content-area">
+            <main id="main" class="site-main">
+                <nav id="filtrering">
+                    <button data-is="alle">alle</button>
+                </nav>
+                <section class="iscontainer"></section>
+            </main>
+            <script>
+                let isene;
+                let categories;
+                let filterIs = "alle";
 
-            const dbUrl = "http://helenajakobsen.com/02_kea/02_semester/eksamen%20/popseco/index.php/wp-json/wp/v2/is?per_page=100";
+                const dbUrl = "http://helenajakobsen.com/02_kea/02_semester/eksamen%20/popseco/index.php/wp-json/wp/v2/is?per_page=100";
 
-            const catUrl = "http://helenajakobsen.com/02_kea/02_semester/eksamen%20/popseco/index.php/wp-json/wp/v2/categories";
+                const catUrl = "http://helenajakobsen.com/02_kea/02_semester/eksamen%20/popseco/index.php/wp-json/wp/v2/categories";
 
-            async function getJSON() {
-                const data = await fetch(dbUrl);
-                const catdata = await fetch(catUrl);
+                async function getJSON() {
+                    const data = await fetch(dbUrl);
+                    const catdata = await fetch(catUrl);
 
-                isene = await data.json();
-                categories = await catdata.json();
+                    isene = await data.json();
+                    categories = await catdata.json();
 
-                console.log(categories);
-                visIsene();
-                opretKnapper();
-            }
+                    console.log(categories);
+                    visIsene();
+                    opretKnapper();
+                }
 
-            function opretKnapper() {
-                categories.forEach(cat => {
-                    document.querySelector("#filtrering").innerHTML += `<button class="filter" data-is="${cat.id}">${cat.name}</button>`
-                })
-                addEventListenersToButtons();
-            };
+                function opretKnapper() {
+                    categories.forEach(cat => {
+                        document.querySelector("#filtrering").innerHTML += `<button class="filter" data-is="${cat.id}">${cat.name}</button>`
+                    })
+                    addEventListenersToButtons();
+                };
 
-            function addEventListenersToButtons() {
-                document.querySelectorAll("#filtrering button").forEach(elm => {
-                    elm.addEventListener("click", filtrering);
-                })
-            };
+                function addEventListenersToButtons() {
+                    document.querySelectorAll("#filtrering button").forEach(elm => {
+                        elm.addEventListener("click", filtrering);
+                    })
+                };
 
-            function filtrering() {
-                filterIs = this.dataset.is;
-                console.log(filterIs);
+                function filtrering() {
+                    filterIs = this.dataset.is;
+                    console.log(filterIs);
 
-                visIsene();
-            }
+                    visIsene();
+                }
 
-            function visIsene() {
-                console.log(isene);
-                let temp = document.querySelector("template");
-                let container = document.querySelector(".iscontainer");
-                container.innerHTML = "";
-                isene.forEach(is => {
-                    if (filterIs == "alle" || is.categories.includes(parseInt(filterIs))) {
-                        let klon = temp.cloneNode(true).content;
-                        klon.querySelector(".billede").src = is.billede.guid;
-                        klon.querySelector(".isensnavn").textContent = is.title.rendered;
-                        klon.querySelector(".pris").innerHTML = is.pris;
+                function visIsene() {
+                    console.log(isene);
+                    let temp = document.querySelector("template");
+                    let container = document.querySelector(".iscontainer");
+                    container.innerHTML = "";
+                    isene.forEach(is => {
+                        if (filterIs == "alle" || is.categories.includes(parseInt(filterIs))) {
+                            let klon = temp.cloneNode(true).content;
+                            klon.querySelector(".billede").src = is.billede.guid;
+                            klon.querySelector(".isensnavn").textContent = is.title.rendered;
+                            klon.querySelector(".pris").innerHTML = is.pris;
 
 
-                        klon.querySelector("article").addEventListener("click", () => {
-                            location.href = is.link;
-                        })
-                        container.appendChild(klon);
-                    }
-                })
+                            klon.querySelector("article").addEventListener("click", () => {
+                                location.href = is.link;
+                            })
+                            container.appendChild(klon);
+                        }
+                    })
 
-            }
-            getJSON();
+                }
+                getJSON();
 
-        </script>
+            </script>
 
-    </section>
+        </section>
+    </div>
 </div>
 <?php get_footer(); ?>
--- popseco_child/single-is.php
+++ popseco_child/single-is.php
@@ -1,4 +1,5 @@
-<?php
+<div id="singleis">
+    <?php
 /**
  * Author:          Andrei Baicus <andrei@themeisle.com>
  * Created on:      28/08/2018
@@ -11,58 +12,60 @@ $container_class = apply_filters( 'neve_container_class_filter', 'container', 's
 get_header();
 
 ?>
-<div class="<?php echo esc_attr( $container_class ); ?> single-post-container">
-    <section id="primary" class="content-area">
-        <main id="main" class="site-main">
+    <div class="<?php echo esc_attr( $container_class ); ?> single-post-container">
+        <section id="primary" class="content-area">
+            <main id="main" class="site-main">
 
 
-            <div id="tilbage_knap">
-                <button class="valgt">Tilbage</button>
-            </div>
-            <article class="is_article">
-                <img src="" alt="" class="billede">
-                <div class="text_div2">
-                    <h2 class="navn"></h2>
-                    <h2 class="prissen"></h2>
-                    <p class="beskrivelse"></p>
+                <div id="tilbage_knap">
+                    <button class="valgt">Tilbage</button>
                 </div>
-            </article>
-        </main>
+                <article class="is_article">
+                    <img src="" alt="" class="billede">
+                    <div class="text_div2">
+                        <h2 class="navn"></h2>
+                        <h2 class="prissen"></h2>
+                        <p class="beskrivelse"></p>
+                    </div>
+                </article>
+            </main>
 
-        <script>
-            let pod;
+            <script>
+                let pod;
 
-            let denvalgteis = <?php echo get_the_ID() ?>;
-            const dbUrl = "http://helenajakobsen.com/02_kea/02_semester/eksamen%20/popseco/index.php/wp-json/wp/v2/is/" + denvalgteis;
-
-
-            async function getJSON() {
-                const data = await fetch(dbUrl);
-                is = await data.json();
-                visIs();
-            }
-
-            function visIs() {
-                console.log(is.billede.guid);
-                document.querySelector(".navn").textContent = is.title.rendered;
-                document.querySelector(".billede").src = is.billede.guid;
-                document.querySelector(".beskrivelse").textContent = is.beskrivelse;
-                document.querySelector(".prissen").innerHTML = is.pris;
-                document.querySelector(".valgt").addEventListener("click", tilbageTilMenu);
-            }
-
-            function tilbageTilMenu() {
-                console.log("tilbageTilMenu");
-                history.back();
-
-            }
-
-            getJSON();
-
-        </script>
+                let denvalgteis = <?php echo get_the_ID() ?>;
+                const dbUrl = "http://helenajakobsen.com/02_kea/02_semester/eksamen%20/popseco/index.php/wp-json/wp/v2/is/" + denvalgteis;
 
 
-    </section>
+                async function getJSON() {
+                    const data = await fetch(dbUrl);
+                    is = await data.json();
+                    visIs();
+                }
+
+                function visIs() {
+                    console.log(is.billede.guid);
+                    document.querySelector(".navn").textContent = is.title.rendered;
+                    document.querySelector(".billede").src = is.billede.guid;
+                    document.querySelector(".beskrivelse").textContent = is.beskrivelse;
+                    document.querySelector(".prissen").innerHTML = is.pris;
+                    document.querySelector(".valgt").addEventListener("click", tilbageTilMenu);
+                }
+
+                function tilbageTilMenu() {
+                    console.log("tilbageTilMenu");
+                    history.back();
+
+                }
+
+                getJSON();
+
+            </script>
+
+
+        </section>
+    </div>
 </div>
 <?php
+
 get_footer();
